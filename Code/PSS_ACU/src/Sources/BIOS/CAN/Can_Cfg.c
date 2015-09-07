be080b81b979d126c496f3cc23ca3535961488df
Roberto Palos <roberto.palos.sandoval@gmail.com>
2015-09-07 14:19:46 -0500

    Can configuration update. Modification of table message and baud rate set to 500kbps.

--- Code/PSS_ACU/src/Sources/BIOS/CAN/Can_Cfg.c
+++ Code/PSS_ACU/src/Sources/BIOS/CAN/Can_Cfg.c
@@ -1,30 +1,43 @@
-/*~A*/
-/*~+:Header*/
-/*******************************************************************************/
-/**
-\file       can_cfg.c
-\author     Alicia Álvarez
-\version    1.0
-*/
-/****************************************************************************************************/
-/*~E*/
-/*~A*/
-/*~+:Imports*/
+/*============================================================================*/
+/*                        SV C CE SOFTWARE GROUP                              */
+/*============================================================================*/
+/*                        OBJECT SPECIFICATION                                */
+/*============================================================================*
+* C Source:         %Can_Cfg.c%
+* Instance:         CAN_1
+* %version:         1.1 %
+* %created_by:      Alicia Alvarez %
+* %date_created:     %
+*=============================================================================*/
+/* DESCRIPTION : Can configuration file.                                      */
+/*============================================================================*/
+/* FUNCTION COMMENT : Can baudrate and messages configuration.				  */
+/*                                                                            */
+/*============================================================================*/
+/*                               OBJECT HISTORY                               */
+/*============================================================================*/
+/*  REVISION |   DATE      |                               |      AUTHOR      */
+/*----------------------------------------------------------------------------*/
+/*  1.0      | xx/xx/xx    |                               | Alicia Alvarez   */
+/* Creation of file.			                                              */
+/*----------------------------------------------------------------------------*/
+/*  1.1      | 07/09/2015  |                               | Roberto Palos    */
+/* Changed documentation format for the templates used in our projects.       */
+/*        */
+/*============================================================================*/
+/* Includes */
+/* -------- */
+
 #include "Can_Cfg.h"
 #include "Can_Manager.h"
-/*~E*/
-/*~A*/
-/*~+:Defines*/
+/* Functions macros, constants, types and datas         */
+/* ---------------------------------------------------- */
+/* Functions macros */
 #define MAX_NUM_RX_FILTERS  (4)
 #define MAX_NUM_TX_FILTERS  (2)
-/*~E*/
-/*~A*/
-/*~+:External Interfaces*/
+/* Exported functions prototypes */
+/* ----------------------------- */
 extern uint8_t CAN_SendFrameInt(uint8_t can_eng, uint8_t can_channel, uint8_t *can_msg_buf, uint8_t len);
-/*~E*/
-/*~A*/
-/*~+:Variables*/
-/*~A*/
 /*~+:CAN Baudrate Configuration*/
 CAN_DevBaudrateType bios_cnf_can_baudrate_options[] = {
 /***********************************************************************************/
@@ -47,14 +60,18 @@ CAN_DevBaudrateType bios_cnf_can_baudrate_options[] = {
 /*~A*/
 /*~+:CAN Messages Configuration*/
 CAN_ConfigMsgType bios_cnf_can_messages[] = {
-{ 0, CNF_CAN_CH_RX, CNF_CAN_DATA_FRAME, CNF_CAN_IDE_STD, 0x1A0,         0x1FF,      Can_Manager_PduHandler0  },
-{ 1, CNF_CAN_CH_RX, CNF_CAN_DATA_FRAME, CNF_CAN_IDE_EXT, 0x12345600,    0x1FFFFFF8, Can_Manager_PduHandler6  }
-/*{ 2, CNF_CAN_CH_RX, CNF_CAN_DATA_FRAME, CNF_CAN_IDE_STD, 0x1B0,         0x000,      NULL  },
-{ 3, CNF_CAN_CH_RX, CNF_CAN_DATA_FRAME, CNF_CAN_IDE_EXT, 0x13579B00,    0x00000000, NULL  },
-{ 4, CNF_CAN_CH_TX, CNF_CAN_DATA_FRAME, CNF_CAN_IDE_STD, 0x1A7,         0x1F8,      NULL  },
-{ 5, CNF_CAN_CH_TX, CNF_CAN_DATA_FRAME, CNF_CAN_IDE_EXT, 0x12345604,    0x1FFFFFF8, NULL  },
-{ 6, CNF_CAN_CH_TX, CNF_CAN_DATA_FRAME, CNF_CAN_IDE_STD, 0x052,         0x1F7,      NULL  },
-{ 7, CNF_CAN_CH_TX, CNF_CAN_DATA_FRAME, CNF_CAN_IDE_EXT, 0x123456AA,    0x1FFFFFF8, NULL  }*/
+/*Eng_RPM	ECM	ACU	0x100	2	RPM_MSB	RPM_LSB	Engine_Active_Status*/
+{0,	CNF_CAN_CH_RX,	CNF_CAN_DATA_FRAME,	CNF_CAN_IDE_STD,	0x100,	0x1FF,	Can_Manager_Eng_RPM},
+/*Veh_Speed	SIM/AVT	ID/ACU	0x201	2	ACTENG_MSB	ACTENG_LSB*/
+{1,	CNF_CAN_CH_RX,	CNF_CAN_DATA_FRAME,	CNF_CAN_IDE_STD,	0x201,	0x1FF,	Can_Manager_Veh_Speed},
+/*Odometer	SIM/AVT	ID/ACU	0x202	2	Increase_ACK	Odometer_Value*/
+{2,	CNF_CAN_CH_RX,	CNF_CAN_DATA_FRAME,	CNF_CAN_IDE_STD,	0x202,	0x1FF,	Can_Manager_Odometer},
+/*Indication	BCM	ID/ACU	0x204	5	Door_Status	High_Beams_Status	Brake_Status*/
+{3,	CNF_CAN_CH_RX,	CNF_CAN_DATA_FRAME,	CNF_CAN_IDE_STD,	0x204,	0x1FF,	Can_Manager_Indication},
+/*SeatBeltIndicator	ACU	ID	0x601	7	Driver_Indication	Driver_Ind_Duty	Driver_Ind_Period	Passenger_Indication	Passenger_Ind_Duty	Passenger_Ind_Period	Driver_Auxiliary_Request*/
+{4,	CNF_CAN_CH_TX,	CNF_CAN_DATA_FRAME,	CNF_CAN_IDE_STD,	0x601,	0x1FF,	NULL},
+/*Chime_Request	ACU	ID	0x602	6	Sound_Tone	Sound_Cadence_Period	Repetitions	Duty Cycle	SoundLocation_Driver	SoundLocation_Passenger*/
+{5,	CNF_CAN_CH_TX,	CNF_CAN_DATA_FRAME,	CNF_CAN_IDE_STD,	0x602,	0x1FF,	NULL}
 };
 
 /*~E*/
