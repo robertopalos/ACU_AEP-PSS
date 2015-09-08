6cff4a77268923a7e89ab15c4458e6ef3c878a72
Roberto Palos <roberto.palos.sandoval@gmail.com>
2015-09-08 10:48:11 -0500

    Can manager update to read and send the messages used in the practice.

--- Code/PSS_ACU/src/Sources/Services/Can_Manager/Can_Manager.c
+++ Code/PSS_ACU/src/Sources/Services/Can_Manager/Can_Manager.c
@@ -1,85 +1,125 @@
-/*~A*/
-/*~+:Module Header*/
-/*******************************************************************************/
-/**
-\file       Can_Manager.c
-\brief      Provide Can Services
-\author     Francisco Martinez
-\version    1.0
-\date       16/08/2015
-*/
-/*******************************************************************************/
-/*~E*/
-/*~A*/
-/*~+:Import*/
+/*============================================================================*/
+/*                        SV C CE SOFTWARE GROUP                              */
+/*============================================================================*/
+/*                        OBJECT SPECIFICATION                                */
+/*============================================================================*
+* C Source:         %Can_Manager.c%
+* Instance:         RPL_1
+* %version:         2 %
+* %created_by:      uid02495 %
+* %date_created:    Sun Aug  16 2015 %
+*=============================================================================*/
+/* DESCRIPTION : Provide Can Services.                                       */
+/*============================================================================*/
+/* FUNCTION COMMENT : Can Driver Adapted to AEP Final Practice.               */
+/*============================================================================*/
+/*                               OBJECT HISTORY                               */
+/*============================================================================*/
+/*  REVISION |   DATE      |                               |      AUTHOR      */
+/*----------------------------------------------------------------------------*/
+/*  1.0      | 16/08/2015  |                               |Francisco Martinez*/
+/* Can Driver provider by Francisco Martinez                                  */
+/*----------------------------------------------------------------------------*/
+/*  1.1      | 08/09/2015  |                               |  Roberto Palos   */
+/*	Adapted CAN IDs, CAN function to response and Pdu_Handlers to read datas. */
+/*============================================================================*/
+
+/* Includes */
+/* -------- */
 #include "Can_Manager.h"
-#include "GPIO.h"
+#include "GPIO.h" 
+#include "DriverReminder.h"
+#include "PassengerReminder.h"
+/*======================================================*/ 
+/* Definition of RAM variables                          */
+/*======================================================*/ 
 
-/*~E*/
-/*~A*/
-/*~+:Defines*/
-
-/*~E*/
-/*~A*/
-/*~+:Variables*/
-struct CanTest
-{
-uint8_t     can_test_enable;
+/* LONG and STRUCTURE RAM variables */
+struct CanTest{
+	uint8_t     can_test_enable;
 }CanTestStruct;
+
 /* Temporary CAN Messages */
-uint8_t dummy_msg0[8] = {0xCA,0x83,0x15,0x77,0x19,0x56,0x65,0x00};
-uint8_t dummy_msg1[8] = {0x00,0x65,0x56,0x19,0x77,0x15,0x83,0xCA};
-uint8_t dummy_msg2[8] = {0x33,0x44,0x55,0x66,0x88,0x89,0x45,0x4C};
-CAN_PduType    pdu_handler4 = { 4, 8, dummy_msg0};
-CAN_PduType    pdu_handler5 = { 5, 6, dummy_msg1};
-CAN_PduType    pdu_handler6 = { 6, 2, dummy_msg2};
-CAN_PduType    pdu_handler1 = { 7, 4, dummy_msg2};
-uint32_t PduHandlerCnt0 = 0;
-uint32_t PduHandlerCnt6 = 0;
-/*~E*/
-/*~A*/
-/*~+:Private Operations*/
+uint8_t SeatBeltIndicator[7] = {10, 20, 30, 40 ,60, 70, 50};
+uint8_t ChimeRequest[6];
 
-/*~E*/
-/*~A*/
-/*~+:Public Operations*/
-/*~A*/
+CAN_PduType	 pdu_handlerSeatbelt = { 4, 7, SeatBeltIndicator};
+CAN_PduType	pdu_handlerChime = { 5, 6, ChimeRequest};
+
+
 /*~+:Can Manager Callbacks*/
-CAN_MessageDataType CanMessage_PduHandler0;
-CAN_MessageDataType CanMessage_PduHandler6;
-void Can_Manager_PduHandler0(CAN_MessageDataType CanMessage)
-{
-CanMessage_PduHandler0 = CanMessage;
-LED_TOGGLE(LED1);
+CAN_MessageDataType CanMessage_Eng_RPM;
+CAN_MessageDataType CanMessage_Veh_Speed;
+CAN_MessageDataType CanMessage_Odometer;
+CAN_MessageDataType CanMessage_Indication;
 
+/* Exported functions */
+/* ------------------ */
+
+/*Can Receive*/
+
+
+/**************************************************************
+ *  Name                 :	export_func
+ *  Description          :
+ *  Parameters           :  [Input, Output, Input / output]
+ *  Return               :
+ *  Critical/explanation :    [yes / No]
+ **************************************************************/
+/*Message	Transmitter	Receiver	ID		DLC	B0		B1		B2						SendType	Cycle
+Eng_RPM		ECM			ACU			0x100	2	RPM_MSB	RPM_LSB	Engine_Active_Status	Periodic	10 ms*/
+void Can_Manager_Eng_RPM(CAN_MessageDataType CanMessage){
+	CanMessage_Eng_RPM = CanMessage;
+	rub_EngineStatus = CanMessage_Eng_RPM.msg_data_field[2];
+}
+/**************************************************************
+ *  Name                 :	export_func
+ *  Description          :
+ *  Parameters           :  [Input, Output, Input / output]
+ *  Return               :
+ *  Critical/explanation :    [yes / No]
+ **************************************************************/
+/*Message	Transmitter	Receiver	ID		DLC	B0			B1			SendType	Cycle
+Veh_Speed	SIM/AVT		ID/ACU		0x201	2	ACTENG_MSB	ACTENG_LSB	Periodic	50 ms*/
+void Can_Manager_Veh_Speed(CAN_MessageDataType CanMessage){
+	CanMessage_Veh_Speed = CanMessage;
+	rub_Speed = 0;
+	rub_Speed =CanMessage_Veh_Speed.msg_data_field[0];
+	rub_Speed = rub_Speed << 8;
+	rub_Speed =(T_UWORD) (rub_Speed + CanMessage_Veh_Speed.msg_data_field[1]);
+	DR_StateMachineEn();
 }
 
-void Can_Manager_PduHandler6(CAN_MessageDataType CanMessage)
-{
-CanMessage_PduHandler6 = CanMessage;
-LED_TOGGLE(LED2);
+/*Message	Transmitter	Receiver	ID		DLC	B0				B1				SendType	Cycle
+Odometer	SIM/AVT		ID/ACU		0x202	2	Increase_ACK	Odometer_Value	Periodic	100 ms*/
+void Can_Manager_Odometer(CAN_MessageDataType CanMessage){
+	CanMessage_Odometer = CanMessage;
+	rub_Distance = CanMessage_Odometer.msg_data_field[1];
 }
-/*~E*/
-/*~A*/
-/*~+:Can Manager Periodic Functions*/
-/*~A*/
-/*~+:CanManager_SendMesage_12p5ms*/
-void CanManager_SendMessage_12p5ms(void)
-{
-CAN_SendFrame(pdu_handler4);
-CAN_SendFrame(pdu_handler5);
-if (CanTestStruct.can_test_enable)
-{
-CanTestStruct.can_test_enable = 0;
+
+/*Odometer register the traveled distance*/
+
+
+/*Message	Transmitter	Receiver	ID		DLC	B0			B1					B2				SendType	Cycle
+			BCM			ID/ACU		0x204	5	Door_Status	High_Beams_Status	Brake_Status	Periodic	100 ms*/
+void Can_Manager_Indication(CAN_MessageDataType CanMessage){
+	CanMessage_Indication = CanMessage;
+	rub_DoorStatus = CanMessage_Indication.msg_data_field[0];
 }
+
+
+/* CAN Send*/
+
+/*Message	Transmitter	Receiver	ID		DLC	B0					B1				B2					B3						B4					B5						B6								SendType	Cycle
+			ACU			ID			0x601	7	Driver_Indication	Driver_Ind_Duty	Driver_Ind_Period	Passenger_Indication	Passenger_Ind_Duty	Passenger_Ind_Period	Driver_Auxiliary_Request		Periodic	200ms
+												0 / 1				Duty Cycle		Period				0 / 1					Duty Cycle			Period					*/
+
+void Can_Manager_Send_SeatbeltIndicator(void){
+	CAN_SendFrame(pdu_handlerSeatbelt);
 }
-/*~E*/
-/*~A*/
-/*~+:CanManager_SendMesage_25ms*/
-void CanManager_SendMessage_25ms(void)
-{
-CAN_SendFrame(pdu_handler6);
+
+void Can_Manager_Send_Chime_Request(void){
+	CAN_SendFrame(pdu_handlerChime);
 }
-/*~E*/
-/*~E*/
-/*~E*/
+
+
--- Code/PSS_ACU/src/Sources/Services/Can_Manager/Can_Manager.h
+++ Code/PSS_ACU/src/Sources/Services/Can_Manager/Can_Manager.h
@@ -1,3 +1,30 @@
+/*============================================================================*/
+/*                        SV C BC SOFTWARE GROUP                              */
+/*============================================================================*/
+/*                        OBJECT SPECIFICATION                                */
+/*============================================================================*
+* C Include:        %Can_Manager.h%
+* Instance:         PSS_ACU%
+* %version:         1.2%
+* %created_by:       %
+* %date_created:     %
+*=============================================================================*/
+/* DESCRIPTION : CanManaer header.                                            */
+/*============================================================================*/
+/* FUNCTION COMMENT : Can Manager header with function prototypes to be       */
+/*  exported to the ACU                                                       */
+/*============================================================================*/
+/*                               OBJECT HISTORY                               */
+/*============================================================================*/
+/*  REVISION |   DATE      |                               |      AUTHOR      */
+/*----------------------------------------------------------------------------*/
+/*  1.0      | 16/08/2015 |                              | Francisco Martinez */
+/* Provide Can Services.                                                      */
+/*----------------------------------------------------------------------------*/
+/*  1.2      | 16/08/2015 |                              |   Roberto Palos    */
+/* Removed old prototypes and update with the used in sources.                */
+/* Added variable to be exported to modify and then send in Can Messages.     */
+/*============================================================================*/
 /*~A*/
 /*~+:Module Header*/
 /*******************************************************************************/
@@ -17,10 +44,18 @@
 #include "Can.h"
 /*~E*/
 /*~A*/
+
+extern uint8_t SeatBeltIndicator[7];
+extern uint8_t ChimeRequest[6];
 /*~+:Exported Interfaces*/
-extern void Can_Manager_PduHandler0(CAN_MessageDataType CanMessage);
-extern void Can_Manager_PduHandler6(CAN_MessageDataType CanMessage);
-extern void CanManager_SendMessage_12p5ms(void);
-extern void CanManager_SendMessage_25ms(void);
+extern void Can_Manager_Eng_RPM(CAN_MessageDataType CanMessage);
+extern void Can_Manager_Veh_Speed(CAN_MessageDataType CanMessage);
+extern void Can_Manager_Odometer(CAN_MessageDataType CanMessage);
+extern void Can_Manager_Indication(CAN_MessageDataType CanMessage);
+
+extern void Can_Manager_Send_SeatbeltIndicator(void);
+extern void Can_Manager_Send_Chime_Request(void);
+
+
 /*~E*/
 #endif /* __CAN_MANAGER */
